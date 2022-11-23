eecbc02ea1f879f4a5887074f9c01e8605470cf4
kennguyen230 <kennguyen230@gmail.com>
2022-11-23 14:19:38 -0800

    Initial Commit

---
+++ poop-counter/poop_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  poop_counterApp.swift
+//  poop-counter
+//
+//  Created by Ken Nguyen on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct poop_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
