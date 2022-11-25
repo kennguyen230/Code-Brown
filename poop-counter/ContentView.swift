67935b5da2e048e4d6aed94ce111910d5036babe
kennguyen230 <kennguyen230@gmail.com>
2022-11-24 18:36:02 -0800

    First draft of home screen

--- poop-counter/ContentView.swift
+++ poop-counter/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hola, Poop!")
+        ZStack {
+            Image("Background")
+            VStack{
+                
+                // For the two buttons at the top
+                HStack{
+                    Image(systemName: "chart.bar.xaxis")
+                        .resizable()
+                        .frame(width:28, height:28)
+                    Spacer()
+                        .frame(width:300)
+                    Image(systemName: "calendar.circle.fill")
+                        .resizable()
+                        .frame(width:28, height:28)
+                }
+                .padding()
+                
+                // For the month & day
+                HStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(width: 225, height: 50)
+                            .foregroundColor(Color.gray)
+                        Text("Month x, day y")
+                    }
+                }
+                
+                // For the plus button, minus button, and counter
+                HStack{
+                    Spacer()
+                    Image(systemName:"minus.circle")
+                    Spacer()
+                    Text("46")
+                        .font(.system(size: 96))
+                    Spacer()
+                    Image(systemName:"plus.circle")
+                    Spacer()
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
