d2f4d89698c388f8e8eeb4adeb0d70523173dadd
kennguyen230 <kennguyen230@gmail.com>
2022-11-24 19:01:43 -0800

    First draft of spacing

--- poop-counter/ContentView.swift
+++ poop-counter/ContentView.swift
@@ -18,13 +18,13 @@ struct ContentView: View {
                     Image(systemName: "chart.bar.xaxis")
                         .resizable()
                         .frame(width:28, height:28)
+                        .padding(.leading, 25)
                     Spacer()
-                        .frame(width:300)
                     Image(systemName: "calendar.circle.fill")
                         .resizable()
                         .frame(width:28, height:28)
+                        .padding(.trailing, 25)
                 }
-                .padding()
                 
                 // For the month & day
                 HStack{
@@ -36,15 +36,23 @@ struct ContentView: View {
                     }
                 }
                 
+                Spacer()
+                
                 // For the plus button, minus button, and counter
                 HStack{
                     Spacer()
                     Image(systemName:"minus.circle")
+                        .resizable()
+                        .frame(width:28, height:28)
+                        .opacity(0.7)
                     Spacer()
                     Text("46")
                         .font(.system(size: 96))
                     Spacer()
                     Image(systemName:"plus.circle")
+                        .resizable()
+                        .frame(width:28, height:28)
+                        .opacity(0.7)
                     Spacer()
                 }
                 
