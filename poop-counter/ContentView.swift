11e875247c67f3d6dd1f0e910a55c2872bcb20e7
kennguyen230 <kennguyen230@gmail.com>
2022-11-27 19:36:53 -0800

    Added date; counter is operational

--- poop-counter/ContentView.swift
+++ poop-counter/ContentView.swift
@@ -8,51 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var poop = 0
+    
     var body: some View {
         ZStack {
-            Image("Background")
             VStack{
                 
                 // For the two buttons at the top
                 HStack{
-                    Image(systemName: "chart.bar.xaxis")
-                        .resizable()
-                        .frame(width:28, height:28)
-                        .padding(.leading, 25)
+                    Button{
+                        StatsView()
+                        print("herro")
+                    } label: {
+                        Image(systemName: "chart.bar.xaxis")
+                            .resizable()
+                            .frame(width:30, height:30)
+                            .padding(.leading, 25)
+                            .padding(.top, 25)
+                    }
+                    
                     Spacer()
-                    Image(systemName: "calendar.circle.fill")
-                        .resizable()
-                        .frame(width:28, height:28)
-                        .padding(.trailing, 25)
+                    
+                    Button{
+                        print("lerro")
+                        CalendarView()
+                    } label: {
+                        Image(systemName: "calendar.circle.fill")
+                            .resizable()
+                            .frame(width:30, height:30)
+                            .padding(.trailing, 25)
+                            .padding(.top, 25)
+                    }
+                    
                 }
+                
+                Spacer().frame(height:80)
                 
                 // For the month & day
                 HStack{
                     ZStack{
                         RoundedRectangle(cornerRadius: 25)
-                            .frame(width: 225, height: 50)
+                            .frame(width: 250, height: 50)
                             .foregroundColor(Color.gray)
-                        Text("Month x, day y")
+//                        Text(Date(), style:.date)
+                        Text(Date().formatted(date: .complete, time: .omitted))
+                        
                     }
                 }
                 
-                Spacer()
+                Spacer().frame(height:125)
                 
                 // For the plus button, minus button, and counter
                 HStack{
                     Spacer()
-                    Image(systemName:"minus.circle")
-                        .resizable()
-                        .frame(width:28, height:28)
-                        .opacity(0.7)
+                    
+                    // Minus button
+                    Button{
+                        if(poop == 0){
+                            return
+                        } else {
+                            poop-=1
+                        }
+                    } label: {
+                        Image(systemName:"minus.circle")
+                            .resizable()
+                            .frame(width:28, height:28)
+                            .opacity(0.7)
+                    }
+                    
                     Spacer()
-                    Text("46")
+                    
+                    // Counter
+                    Text(String(poop))
                         .font(.system(size: 96))
+                    
                     Spacer()
-                    Image(systemName:"plus.circle")
-                        .resizable()
-                        .frame(width:28, height:28)
-                        .opacity(0.7)
+                    
+                    // Plus button
+                    Button{
+                        poop+=1
+                    } label: {
+                        Image(systemName:"plus.circle")
+                            .resizable()
+                            .frame(width:28, height:28)
+                            .opacity(0.7)
+                    }
+                    
                     Spacer()
                 }
                 
